fd7f9f349d102b6889556df2b9b610beccd03ecd
fsouza.andreia <fsouza.andreia@gmail.com>
2025-02-10 21:18:18 -0300

    feat: layout main page buttons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,27 @@
 import Logo from "@/components/Logo/Logo";
 import * as S from "./styles";
-import { Link } from "react-router-dom";
+import DefaultBtn from "@/components/Buttons/DefaultBtn";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <S.Main>
       <Logo />
-      <p>
-        <Link to="/cartas">Cartas</Link>
-      </p>
-      <p>
-        <Link to="play">Jogue agora</Link>
-      </p>
+      <S.ButtonWrapper>
+        <DefaultBtn
+          text="Ver Cartas"
+          size="small"
+          color="primary"
+          onClick={() => navigate("/cartas")}
+        />
+        <DefaultBtn
+          text="Jogue Agora"
+          size="small"
+          color="primary"
+          onClick={() => navigate("/play")}
+        />
+      </S.ButtonWrapper>
     </S.Main>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  padding: 4rem 0;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-top: 2rem;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+`;
